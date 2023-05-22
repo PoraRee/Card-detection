f65ee58ee9d1f4e91b6862480567542acc04f261
Pipat Saengow <pay2630@gmail.com>
2023-05-23 04:24:20 +0700

    segmentor update

--- module/Segmentor.py
+++ module/Segmentor.py
@@ -1,4 +1,5 @@
 import cv2
+import cv2 as cv
 import math
 import numpy as np
 from numpy import random
@@ -8,6 +9,77 @@ from ipywidgets import interact
 import ipywidgets as widgets
 import urllib.request
 import time
+
+
+def quad(contour):
+    minx, maxx = contour[:, :, 0].min(), contour[:, :, 0].max()
+    miny, maxy = contour[:, :, 1].min(), contour[:, :, 1].max()
+    
+    basemask = np.zeros((maxy-miny, maxx-minx), dtype='uint8')
+    cv.fillConvexPoly(basemask, contour - np.array([[minx, miny]]), 1)
+    
+    bestHull = None
+    bestHullIoU = 0
+    for i in range(10):
+        c = np.random.choice(np.arange(len(contour)), 4, replace=False)
+        hull = cv.convexHull(contour[c])
+        if len(hull) != 4:
+            continue
+        simmask = np.zeros_like(basemask)
+        cv.fillConvexPoly(simmask, hull - np.array([[minx, miny]]), 1)
+        iou = (simmask & basemask).sum()/(simmask | basemask).sum()
+        if  iou > bestHullIoU:
+            bestHullIoU = iou
+            bestHull = hull            
+    return bestHull, bestHullIoU
+
+def drawHull(contour, alpha):
+    minx, maxx = contour[:, :, 0].min(), contour[:, :, 0].max()
+    miny, maxy = contour[:, :, 1].min(), contour[:, :, 1].max()
+    basemask = np.zeros((maxy-miny, maxx-minx), dtype='uint8')
+    cv.fillConvexPoly(basemask, contour - np.array([[minx, miny]]), 1)
+    plt.imshow(basemask, alpha=alpha)
+
+def cardProject(img, quad):
+    dst = np.array([
+        [0, 100],
+        [0, 0],
+        [80, 0],
+        [80, 100],
+    ], dtype='float32')
+    if np.sqrt(((quad[0, 0, :] - quad[1, 0, :])**2).sum()) < np.sqrt(((quad[1, 0, :] - quad[2, 0, :])**2).sum()) :
+        quad = np.concatenate([quad[1:], quad[0:1]])
+    mtx = cv.getPerspectiveTransform(quad.astype('float32'), dst)
+    timg= cv.warpPerspective(img, mtx, (80, 100))
+    return timg
+
+class Segmentor2:
+    def __init__(self, iou_thresh=0.75):
+        self.iou_thresh = iou_thresh
+
+    def seg(self, image):
+        img = image
+        _, mask = cv.threshold(cv.cvtColor(img, cv.COLOR_BGR2GRAY), 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+        contours, _ = cv.findContours(mask.astype('uint8'), cv.RETR_EXTERNAL , cv.CHAIN_APPROX_SIMPLE)
+        valid_contours = [cv.approxPolyDP(c, 5, True) for c in contours if cv.contourArea(c) > 3000]
+
+        result = []
+
+        for i, c in enumerate(valid_contours):
+            q, iou = quad(c)
+            if iou < self.iou_thresh:
+                continue
+            out = cardProject(img, q)
+            crop = out[:out.shape[0]//2, :out.shape[1]//2, :]
+            result.append({
+                'bbox': q.squeeze(),
+                'card': out, # Cropped image of card
+                'suit': crop # Cropped image of card corner
+            })
+
+        return result   
+
 
 class Segmentor:
     def __init__(
@@ -122,6 +194,9 @@ class Segmentor:
 if __name__ == "__main__":
     inputImage = cv2.imread("bicyclecard/images/IMG_20230520_185914631.jpg")
 
+    segmentor = Segmentor2()
+    """
+
     segmentor = Segmentor(
         min_p=4,
         max_p=400,
@@ -130,6 +205,7 @@ if __name__ == "__main__":
         suit_x_ratio=2 / 7,
         suit_y_ratio=1 / 5,
     )
+    """
     
     st = time.time()
     results = segmentor.seg(inputImage)
